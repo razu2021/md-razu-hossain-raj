b75021e3e84758b384ecb469ef917a765b37b0ce
Md Razu Hossain Raj <mdrazuhossainraj@gmail.com>
2025-12-06 17:41:08 +0600

    update

--- app/components/home/banner.vue
+++ app/components/home/banner.vue
@@ -1,5 +1,26 @@
 <!-- ========  script section start her ====== -->
 <script setup lang="ts">
+    import { onMounted } from "vue";
+    import Typed from 'typed.js';
+
+    onMounted(() => {
+        const typed = new Typed("#typed", {
+            strings: [
+            "a Web Developer",
+            "a Designer",
+            "a Nuxt.js Expert",
+            "a UI/UX Enthusiast"
+            ],
+            typeSpeed: 60,
+            backSpeed: 40,
+            backDelay: 1000,
+            loop: true,
+            smartBackspace: true,
+            showCursor: true,
+            cursorChar: "|"
+        });
+    });
+
 
 </script>
 
@@ -9,19 +30,29 @@
 <template>
     <section class="index_banner">
         <div class="container">
-            <div class="banner_wrapper">
-                <div class="col-lg-12">
+            <div class="row align-items-center">
+                <div class="col-lg-8 col-md-7 col-12">
                     <div class="banner_content">
-                        <h4> Thank's for your <span> Kind </span></h4>
-                        <h1> HI, Iam Md Razu Hossain Raj A <br>
-                            <span> Full-Stack Webdeveloper </span></h1>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure commodi officiis suscipit voluptatibus corporis rerum quos adipisci rem earum, hic nisi id quam nulla dignissimos consectetur. Quae culpa repellat nisi vel perspiciatis reiciendis animi fugit explicabo quaerat voluptates quas dolore asperiores, non minima voluptatum cumque sequi aut voluptas tempora temporibus.</p>
-                        <button><a href="">Let's know </a></button>
+                        <h4>Thank's for your <span>Kind</span></h4>
+                        <h1>
+                            HI, I'm Md Razu Hossain Raj <br />
+                            <span id="typed"></span>
+                        </h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure commodi
+                            officiis suscipit voluptatibus corporis rerum quos adipisci rem earum.
+                        </p>
+                            <div class="button1_nuxt">
+                                <NuxtLink to="/about">About me</NuxtLink>
+                            </div>
                     </div>
-                    <!-- banner content end -->
-                     <div class="banner_image">
-                        <img src="https://lh3.googleusercontent.com/p/AF1QipNJ3CYJumMNA7q53gF20Ib6oqwnYJjGjxbcjTt3=s680-w680-h510-rw" alt="">
-                     </div>
+                </div>
+
+                <div class="col-lg-4 col-md-5 col-12">
+                    <div class="banner_image ">
+                        <NuxtImg src="/6.png" class="image" />
+                        <h1 class="">Web Developer </h1>
+                    </div>
                 </div>
             </div>
         </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,21 +3,18 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  /**-------  link global css file start here ------- */
+  css:[
+    'bootstrap/dist/css/bootstrap.min.css',
+      "bootstrap-icons/font/bootstrap-icons.css",
+    '~/assets/css/style.css',
+    '~/assets/scss/main.scss',
+  ],
 
+  /**------ link global js file from plugins folder -------- */
+  plugins:[
+    '~/plugins/plugin.client.ts'  
+  ],
 
-/**-------  link global css file start here ------- */
-css:[
-  'bootstrap/dist/css/bootstrap.min.css',
-    "bootstrap-icons/font/bootstrap-icons.css",
-  '~/assets/css/style.css',
-  '~/assets/scss/main.scss',
-],
-
-
-/**------ link global js file from plugins folder -------- */
-plugins:[
-  '~/plugins/plugin.client.ts'  
-]
-
-
+  modules: ['@nuxt/image']
 })
