74812c18b2708dcb37abb4af1c3a25f5c1792054
Md Razu Hossain Raj <84346306+razu2021@users.noreply.github.com>
2025-12-13 22:39:23 +0600

    update

--- app/pages/resume-of-md-razu-hossain-raj.vue
+++ app/pages/resume-of-md-razu-hossain-raj.vue
@@ -7,9 +7,80 @@
 
 <!-- =====  template section start here ========= -->
 <template>
-    <h1>Resumen of Md Razu hossain Raj </h1>
-</template>
+<section class="cv_banner_of">
+   <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="cv_banner">
+                <h1>Cericulam Vite of <br> Md Razu Hossain Raj </h1>
+            </div>
+        </div>
+    </div>
+   </div>
+</section>
+<!-- banner end here -->
+<section class="cv_section">
+    <div class="container ">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="cv_profile">
+                    <div class="cv_image">
+                        <nuxt-img src="/6.png"/>
+                    </div>
+                    <div class="cv_name">
+                        <h2>Md Razu Hossain Raj </h2>
+                        <p>Full-Stack Developer </p>
 
+                    </div>
+                    <div class="cv_social">
+                        <a href="">GitHub</a>
+                        <a href="">LinkedIn</a>
+                    </div>
+                </div>
+            </div>
+            <!-- col end  -->
+                <div class="col-lg-9">
+                    <div class="cv_content">
+                        <div class="summary">
+                            <h4 class="subtitles">CAREER OBJECTIVE</h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut beatae voluptatem quae in consectetur ex tenetur nemo eum aspernatur modi, aperiam fugiat maxime aliquid! Magni obcaecati voluptatem sed earum dolore eaque facere dolorum cupiditate reiciendis tempore, maiores ex, consequatur illo mollitia dolor excepturi neque ratione sapiente, ab quasi et suscipit!</p>
+                        </div>
+                         <h4 class="subtitles">Job Experiences</h4>
+                        <div class="job_experience">
+                            <h4>Univesitas Law Chambers </h4>
+                            <strong> Web Designer & Developer</strong>
+                            <time datetime=""> Sep-2022 <span> to </span> Till now</time>
+                        </div>
+                        <div class="job_experience">
+                            <h4>Univesitas Law Chambers </h4>
+                            <strong> Web Designer & Developer</strong>
+                            <time datetime=""> Sep-2022 <span> to </span> Till now</time>
+                        </div>
+                        <div class="job_experience">
+                            <h4>Univesitas Law Chambers </h4>
+                            <strong> Web Designer & Developer</strong>
+                            <time datetime=""> Sep-2022 <span> to </span> Till now</time>
+                        </div>
+                        <div class="job_experience">
+                            <h4>Univesitas Law Chambers </h4>
+                            <strong> Web Designer & Developer</strong>
+                            <time datetime=""> Sep-2022 <span> to </span> Till now</time>
+                        </div>
+
+
+                    </div>
+                </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+  
+</template>
 <!-- ======== style section start here ========== -->
  <style scoped>
 
